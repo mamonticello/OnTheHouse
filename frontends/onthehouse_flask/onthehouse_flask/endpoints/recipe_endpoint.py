959877b3e78cbca7871d2a483370e7ce12f0934d
mamonticello <micah.monticello@gmail.com>
2018-02-09 01:04:55 -0800

    search function in progress

--- frontends/onthehouse_flask/onthehouse_flask/endpoints/recipe_endpoint.py
+++ frontends/onthehouse_flask/onthehouse_flask/endpoints/recipe_endpoint.py
@@ -17,3 +17,14 @@ def recipes():
     recipes = common.rdb.get_recipes()
     response = render_template("recipes.html", recipes=recipes)
     return response
+
+
+@site.route('/recipe/search')
+def recipes_search():
+    ingredients = request.args.get('ingredients', None)
+    if ingredients is not None:
+        ingredients = ingredients.split('+')
+        ingredients = [common.rdb.get_ingredient(name=ing) for ing in ingredients]
+    results = common.rdb.search(ingredients=ingredients)
+    response = render_template("recipes.html", results=results)
+    return response
