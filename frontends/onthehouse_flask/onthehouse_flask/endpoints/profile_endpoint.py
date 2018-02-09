12d9f6b6d5c61bc2d7da1ac40479c51ba428f051
Ethan Dalool <edalool@yahoo.com>
2018-02-09 00:05:41 -0800

    Add new URL handler for profile pics.

--- frontends/onthehouse_flask/onthehouse_flask/endpoints/profile_endpoint.py
+++ frontends/onthehouse_flask/onthehouse_flask/endpoints/profile_endpoint.py
@@ -1,18 +1,27 @@
-from flask import request, render_template
+import flask; from flask import request, render_template
 
 from . import common
 
 site = common.site
 
 
-@site.route('/user/<profileid>')
-def get_user(profileid):
-    user = common.rdb.get_user(id=profileid)
+@site.route('/user/<username>')
+def get_user(username):
+    user = common.rdb.get_user(username=username)
     recipes = common.rdb.search(author=user)
-    profileimg = common.rdb.get_image(user.ProfileImageID)
-    response = render_template("profile.html", user=user, recipes=recipes, profileimg = profileimg)
+    response = render_template("profile.html", user=user, recipes=recipes)
     return response
 
+
+@site.route('/user/<username>/profilepic')
+@site.route('/user/<username>/profilepic.<ext>')
+def get_profile_pic(username, ext=None):
+    user = common.rdb.get_user(username=username)
+    if user.profile_pic:
+        return flask.send_file(user.profile_pic.file_path)
+    else:
+        path = common.STATIC_DIR.with_child('default_profile_pic.jpg').absolute_path
+        return flask.send_file(path)
 
 @site.route('/user')
 def user():
